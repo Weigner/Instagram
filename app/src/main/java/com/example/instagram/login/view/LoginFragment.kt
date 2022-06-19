9ca9983c084f36c7b36f027c06a7d843d7b13a78
Erick Weigner Freitas Cunha <erickweiner@gmail.com>
2022-06-19 18:48:37 -0300

    Testando tag button personalizado

--- app/src/main/java/com/example/instagram/login/view/LoginFragment.kt
+++ app/src/main/java/com/example/instagram/login/view/LoginFragment.kt
@@ -1,6 +1,10 @@
 package com.example.instagram.login.view
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -24,7 +28,36 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.ilEmail.error = "email invalido"
-        binding.etPassword.error = "email invalido"
+        addListener()
+    }
+
+    private fun addListener() {
+        val watcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                binding.btEnter.isEnabled = s.toString().isNotEmpty()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+        }
+
+        binding.apply {
+            etEmail.addTextChangedListener(watcher)
+            btEnter.setOnClickListener {
+                btEnter.showProgress()
+
+                //simulando um delay de requisição no backend
+                Handler(Looper.getMainLooper()).postDelayed({
+                    btEnter.showProgress(false)
+                    ilEmail.error = "erro"
+                    ilPassword.error = "erro"
+                }, 5000)
+            }
+        }
     }
 }
