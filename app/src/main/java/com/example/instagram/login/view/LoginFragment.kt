3a31488ef745e9f2f3ece3a695051351bbc5c300
Erick Weigner Freitas Cunha <erickweiner@gmail.com>
2022-06-18 22:37:04 -0300

    Implementando tela de login

--- app/src/main/java/com/example/instagram/login/view/LoginFragment.kt
+++ app/src/main/java/com/example/instagram/login/view/LoginFragment.kt
@@ -5,19 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.instagram.MainActivity
 import com.example.instagram.R
 import com.example.instagram.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
 
     private var _binding: FragmentLoginBinding? = null
-    private val binding get() = _binding
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+    ): View {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }
