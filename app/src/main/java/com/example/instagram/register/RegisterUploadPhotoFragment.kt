3ae88d1bdb150eb927493a8bfaf841935c03241e
Erick Weigner Freitas Cunha <erickweiner@gmail.com>
2022-06-26 14:30:21 -0300

    Criando dialog customizada

--- app/src/main/java/com/example/instagram/register/RegisterUploadPhotoFragment.kt
+++ app/src/main/java/com/example/instagram/register/RegisterUploadPhotoFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.instagram.R
+import com.example.instagram.commom.CustomDialog
 import com.example.instagram.databinding.FragmentRegisterUploadPhotoBinding
 
 class RegisterUploadPhotoFragment : Fragment() {
@@ -18,5 +20,23 @@ class RegisterUploadPhotoFragment : Fragment() {
     ): View {
         _binding = FragmentRegisterUploadPhotoBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val customDialog = CustomDialog(requireContext())
+        customDialog.addButton(R.string.add_photo, R.string.gallery) {
+            when(it.id) {
+                R.string.add_photo -> {
+
+                }
+                R.string.gallery -> {
+
+                }
+            }
+        }
+
+        customDialog.show()
     }
 }
