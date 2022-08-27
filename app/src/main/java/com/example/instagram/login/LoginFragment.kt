25e8ca93303706b489f2705474365b3fe66d3ddf
Erick Weigner Freitas Cunha <erickweiner@gmail.com>
2022-08-26 22:14:14 -0300

    Ajustes no LoginFragment

--- app/src/main/java/com/example/instagram/login/LoginFragment.kt
+++ app/src/main/java/com/example/instagram/login/LoginFragment.kt
@@ -3,13 +3,12 @@ package com.example.instagram.login
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.text.Editable
-import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.instagram.databinding.FragmentLoginBinding
+import com.example.instagram.util.TxtWatcher
 
 class LoginFragment : Fragment() {
 
@@ -26,26 +25,18 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        addListener()
+        setListener()
     }
 
-    private fun addListener() {
-        val watcher = object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+    private fun setListener() {
 
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                binding.btEnter.isEnabled = s.toString().isNotEmpty()
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-
-            }
+        val textWatcher = TxtWatcher {
+            binding.btEnter.isEnabled = it.isNotEmpty()
         }
 
         binding.apply {
-            etEmail.addTextChangedListener(watcher)
+            etEmail.addTextChangedListener(textWatcher)
+            etPassword.addTextChangedListener(textWatcher)
             btEnter.setOnClickListener {
                 btEnter.showProgress()
 
